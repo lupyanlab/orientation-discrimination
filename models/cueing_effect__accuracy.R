aa14ef806339cdddf1066c82257113de52aa3966
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-07-21 16:15:40 -0500

    Answering reviews

--- models/cueing_effect__accuracy.R
+++ models/cueing_effect__accuracy.R
@@ -16,11 +16,28 @@ orientation <- filter(orientation,
                       subj_id %nin% dualmask_outliers,
                       subj_id %nin% temporal_outliers)
 
+# Models predicting error rate (is_error)
+# ---------------------------------------
+# Cueing effect in accuracies (nomask trials)
+cueing_effect_error_mod <- glmer(is_error ~ cue_l + cue_q + (1|subj_id),
+                                 family = binomial,
+                                 data = filter(orientation, mask_type == "nomask"))
+summary(cueing_effect_error_mod)
+# Hearing a verbal cue did not influence accuracy performance
+
+orientation %>%
+  filter(mask_type == "nomask") %>%
+  group_by(cue_type) %>%
+  summarize(error_rate = mean(is_error, na.rm = TRUE))
+
 # Predict rts from mask_type and cue_type
 # ---------------------------------------
+# Collapses across both experiments
 error_mod <- glmer(is_error ~ mask_c * (cue_l + cue_q) + (1|subj_id),
                    family = binomial, data = orientation)
 summary(error_mod)
+
+# Is the reduction in the cueing effect (mask_c:cue_l)
 
 
 # Effect of mask on noise cue trials
