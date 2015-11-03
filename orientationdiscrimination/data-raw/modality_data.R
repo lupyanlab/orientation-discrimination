5e60cac8a071ffd60cdfaaf54fc878be3da87222
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-11-03 16:00:55 -0600

    Process modality data

---
+++ orientationdiscrimination/data-raw/modality_data.R
@@ -0,0 +1,37 @@
+
+# Compile the modality data
+
+devtools::load_all()
+
+modality <- compile("data-raw/modality/data", key = "ODM",
+                    headername = "_header.txt")
+
+# Remove practice trials
+# ----------------------
+modality <- filter(modality, block_ix != -1)
+
+# Exclude RTs on incorrect responses and timeout trials
+# -----------------------------------------------------
+modality$rt <- with(modality, ifelse(is_correct == 0, NA, rt))
+
+# Exclude accuracy on timeout trials
+# ----------------------------------
+modality$is_correct <- with(modality, ifelse(response == "timeout", NA, is_correct))
+
+# Create an is_error column
+# -------------------------
+modality$is_error <- with(modality, ifelse(is_correct == 0, 1, 0))
+
+# Put columns in the correct order
+# --------------------------------
+modality <- modality %>%
+  select(subj_id, block_ix, trial_ix,
+         cue, cue_type,
+         mask_type,
+         pic, up_pic,
+         response,
+         rt,
+         is_correct, is_error) %>%
+  arrange(subj_id, block_ix, trial_ix)
+
+devtools::use_data(modality)
